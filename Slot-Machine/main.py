6b14f755d8f971cdd85459ca09297154adbf9d41
Adebayo Samuel <eladebayoor@gmail.com>
2022-09-24 21:29:57 +0100

    [feat]: get_number_of_lines method created

--- Slot-Machine/main.py
+++ Slot-Machine/main.py
@@ -1,3 +1,5 @@
+MAX_LINES = 3
+
 def deposit():
     """responsible for getting user deposit"""
     while True:
@@ -11,3 +13,16 @@ def deposit():
         else:
             print("Please enter a number.")
     return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines")
+        else:
+            print("Please enter a number.")
+    return lines
